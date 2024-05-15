f65dbfb63cb041f98db6f05de7893e961f8a111d
Daniel Blomkvist <daniel.blomkvist@gmail.com>
2024-05-15 23:28:36 +0200

    Oppdater plantekasse.py slik at den regner ut antall lengder av ulike materialer man må skaffe, ikke bare total lengde man trenger.

--- plantekasse.py
+++ plantekasse.py
@@ -8,20 +8,155 @@ import argparse
 # Dimensjoner på materialer
 SPILE_BREDDE = int(23)
 SPILE_HØYDE = int(48)
+SPILE_LENGDE = int(4200)
 LEKT_BREDDE = int(48)
 LEKT_HØYDE = int(48)
-TOPPBORD_BREDDE = int(148)
-TOPPBORD_HØYDE = int(19)
+LEKT_LENGDE = int(4800)
+TOPPBORD_BREDDE = int(120)
+TOPPBORD_HØYDE = int(28)
+TOPPBORD_LENGDE = int(4500)
 
 AVSTAND_EKSTRA_LEKTER_I_BUNN = int(300)
 
 ØNSKET_OVERHENG_TOPPBORD = int(50)
 
+class Lengde:
+  def __init__(self, lengde):
+      self.lengde = lengde
+      self.next   = None
+
+class LengdeListe:
+  def __init__(self):
+      self.head = None
+      self.listelengde = 0
+
+  def leggTilNyLengde(self, lengde):
+    ny_lengde = Lengde(lengde)
+    self.listelengde += 1
+
+    if self.head is None:
+        self.head = ny_lengde
+    else:
+      current_lengde = self.head
+      while(current_lengde.next):
+          current_lengde = current_lengde.next
+
+      current_lengde.next = ny_lengde
+    print("leggTilNyLengde: La til ny lengde på " + str(lengde) + " mm. Ny listelengde er " + str(self.listelengde))
+    return
+
+  def finnMinsteLangNok(self, ønsket_lengde):
+    if self.head == None:
+        print("finnMinsteLangNok: Ingen element i liste")
+        return None
+
+    index = 0
+    current_lengde = self.head
+    shortest = None
+
+    while(current_lengde != None):
+      print("finnMinsteLangNok: Lengde på index " + str(index) + " har lengdefelt " + str(current_lengde.lengde) + " mm, og ønsket lengde er " + str(ønsket_lengde) + " mm")
+
+      if current_lengde.lengde >= ønsket_lengde:
+        if shortest == None or current_lengde.lengde < shortest:
+          shortest       = current_lengde.lengde
+          shortest_index = index
+          print("finnMinsteLangNok: Oppdaterte kortest til " + str(shortest) + " på indeks " + str(shortest_index))
+
+      current_lengde = current_lengde.next
+      index += 1
+
+    if shortest:
+      print("finnMinsteLangNok: returnerer indeks til korteste som er lang nok: " + str(shortest_index))
+      return shortest_index
+    else:
+      print("finnMinsteLangNok: returnerer None fordi ingen som var lang nok ble funnet")
+      return None
+
+  def fjernLengde(self, remove_index):
+    if self.head == None:
+        print("fjernLengde: Ingen element i liste")
+        return None
+
+    current_lengde = self.head
+    index = 0
+    if index == remove_index:
+        print("fjernLengde: Fjerner første element i listen")
+        self.head  = self.head.next
+    else:
+        while(current_lengde != None and index + 1 != remove_index):
+            current_lengde = current_lengde.next
+            index += 1
+
+        if current_lengde != None:
+            print("fjernLengde: Fjerner element på indeks " + str(index))
+            current_lengde.next = current_lengde.next.next
+        else:
+          print("fjernLengde: Indeks ble ikke funnet")
+          return None
+
+  def kutt(self, kutt_index, kutt_lengde):
+    print("kutt: Skal kutte i index " + str(kutt_index) + " og lengden på kuttet er " + str(kutt_lengde) + " mm")
+    if self.head == None:
+      print("kutt: Ingen element i liste")
+      return None
+
+    current_lengde = self.head
+    index = 0
+
+    while(current_lengde != None and index != kutt_index):
+      current_lengde = current_lengde.next
+      index += 1
+
+    if current_lengde != None:
+      if kutt_lengde < current_lengde.lengde:
+        print("kutt: Ønsket kutt mindre enn lengden til et eksisterende element på indeks " + str(index) +" . Fjerner kuttlengde fra dette elementet.")
+        current_lengde.lengde = current_lengde.lengde - kutt_lengde
+      elif kutt_lengde == current_lengde.lengde:
+        print("kutt: Ønsket kutt identisk med lengden til et eksisterende element på indeks " + str(index) +" . Fjerner elementet.")
+        self.fjernLengde(kutt_index)
+      else:
+        print("kutt: Ønsket kutt lengre enn lengden på lekt/bord")
+    else:
+      print("kutt: Indeks ble ikke funnet")
+
+    return
+
+  def finnTotalLengde(self):
+    if self.head == None:
+        return
+
+    current_lengde = self.head
+    total_lengde = 0
+
+    while(current_lengde != None):
+      total_lengde = total_lengde + current_lengde.lengde
+      current_lengde = current_lengde.next
+
+    return total_lengde
+
+  def print(self):
+    if self.head == None:
+        print("Ingen lengder i liste")
+        return
+
+    print("Skriver ut lengder i liste")
+    current_lengde = self.head
+    index = 0
+
+    while(current_lengde != None):
+      print("Lengde " + str(index) + ": " + str(current_lengde.lengde) + " mm")
+      current_lengde = current_lengde.next
+      index += 1
+
+    return
+
+
 class Plantekasse:
   def __init__(self, intern_lengde, intern_bredde, intern_høyde):
     self.intern_bredde = intern_bredde
     self.intern_lengde = intern_lengde
-    self.intern_høyde = intern_høyde
+    self.intern_høyde  = intern_høyde
 
     # Lengde på breddelekter er samme som breddeparameter
     # antall breddelekter er alltid 6 (2 oppe og 4 nede) + noen ekstra som støtte nederst avhengig av lengden
@@ -56,7 +191,7 @@ class Plantekasse:
         break
       else:
         # Plass til en ny spile.
-        self.ant_spiler_i_høyden = self.ant_spiler_i_høyden + 1
+        self.ant_spiler_i_høyden += 1
         self.høyde_spiler_og_mellomrom = self.høyde_spiler_og_mellomrom + SPILE_HØYDE
 
         # Legg inn gap før neste spile som er like stort som bredden på ei spile
@@ -68,9 +203,138 @@ class Plantekasse:
 
     self.topp_lengde = intern_lengde + 2 * LEKT_BREDDE + 2 * SPILE_BREDDE + 2 * ØNSKET_OVERHENG_TOPPBORD
     self.topp_bredde = intern_bredde + 2 * LEKT_BREDDE + 2 * SPILE_BREDDE + 2 * ØNSKET_OVERHENG_TOPPBORD
-    self.lengde_lekter =  self.ant_breddelekter * self.rammebredde + int(self.ant_breddelekter) * intern_bredde + self.ant_høydelekter * self.høydelekter_lengde
+    self.lengde_lekter =  self.ant_lengdelekter * self.rammelengde + int(self.ant_breddelekter) * intern_bredde + self.ant_høydelekter * self.høydelekter_lengde
     self.lengde_spiler = (2 * self.lengdespile_lengde + 2 * self.breddespile_lengde) * self.ant_spiler_i_høyden
     self.lengde_toppbord = 2 * (self.topp_lengde + self.topp_bredde)
+
+
+    # Lag ei liste over dimensjonene og sorter dem med største først
+    dimension_list = [{"bredde": self.intern_bredde, "lengde": self.intern_lengde, "høyde": self.intern_høyde}]
+    dimension_list = dict(sorted(dimension_list[0].items(), key=lambda item: item[1], reverse=True))
+
+    # Regn ut hvor mange lengder som trengs av hver type lekt/bord
+    self.antall_lekter_nødvendig   = 0
+    self.antall_spiler_nødvendig   = 0
+    self.antall_toppbord_nødvendig = 0
+    self.lekter_liste   = LengdeListe()
+    self.spiler_liste   = LengdeListe()
+    self.toppbord_liste = LengdeListe()
+    for i in dimension_list:
+      print(i)
+      if i == "bredde":
+        # Lekter
+        for j in range(int(self.ant_breddelekter)):
+          print(str(i) + ": " + str(j))
+          # Søk etter minste som er lang nok.
+          funnet_index = self.lekter_liste.finnMinsteLangNok(self.intern_bredde)
+          if funnet_index == None:
+            print("init: fant ikke noen lange nok")
+            # Legg til ny full-lengde lekt
+            self.lekter_liste.leggTilNyLengde(LEKT_LENGDE)
+            # Øk teller på antall lekter nødvendig
+            self.antall_lekter_nødvendig += 1
+            print("init: Oppdaterte antall lekter nødvendig til " + str(self.antall_lekter_nødvendig))
+            # Søk igjen
+            funnet_index = self.lekter_liste.finnMinsteLangNok(self.intern_bredde)
+          # Utfør kuttet
+          self.lekter_liste.kutt(funnet_index, self.intern_bredde)
+
+        # Spiler
+        for j in range(2 * self.ant_spiler_i_høyden):
+          print(str(i) + ": " + str(j))
+          # Søk etter minste som er lang nok.
+          funnet_index = self.spiler_liste.finnMinsteLangNok(self.breddespile_lengde)
+          if funnet_index == None:
+            print("init: fant ikke noen lange nok")
+            # Legg til ny full-lengde spile
+            self.spiler_liste.leggTilNyLengde(SPILE_LENGDE)
+            # Øk teller på antall spiler nødvendig
+            self.antall_spiler_nødvendig += 1
+            print("init: Oppdaterte antall spiler nødvendig til " + str(self.antall_spiler_nødvendig))
+            # Søk igjen
+            funnet_index = self.spiler_liste.finnMinsteLangNok(self.breddespile_lengde)
+          # Utfør kuttet
+          self.spiler_liste.kutt(funnet_index, self.breddespile_lengde)
+
+        # Toppbord
+        for j in range(2):
+          print(str(i) + ": " + str(j))
+          # Søk etter minste som er lang nok.
+          funnet_index = self.toppbord_liste.finnMinsteLangNok(self.topp_bredde)
+          if funnet_index == None:
+            print("init: fant ikke noen lange nok")
+            # Legg til ny full-lengde toppbord
+            self.toppbord_liste.leggTilNyLengde(TOPPBORD_LENGDE)
+            # Øk teller på antall toppbord nødvendig
+            self.antall_toppbord_nødvendig += 1
+            print("init: Oppdaterte antall toppbord nødvendig til " + str(self.antall_toppbord_nødvendig))
+            # Søk igjen
+            funnet_index = self.toppbord_liste.finnMinsteLangNok(self.topp_bredde)
+          # Utfør kuttet
+          self.toppbord_liste.kutt(funnet_index, self.topp_bredde)
+      elif i == "lengde":
+        # Lekter
+        for j in range(self.ant_lengdelekter):
+          # Søk etter minste som er lang nok.
+          funnet_index = self.lekter_liste.finnMinsteLangNok(self.rammelengde)
+          if funnet_index == None:
+            print("init: fant ikke noen lange nok")
+            # Legg til ny full-lengde lekt
+            self.lekter_liste.leggTilNyLengde(LEKT_LENGDE)
+            # Øk teller på antall lekter nødvendig
+            self.antall_lekter_nødvendig += 1
+            print("init: Oppdaterte antall lekter nødvendig til " + str(self.antall_lekter_nødvendig))
+            # Søk igjen
+            funnet_index = self.lekter_liste.finnMinsteLangNok(self.rammelengde)
+          # Utfør kuttet
+          self.lekter_liste.kutt(funnet_index, self.rammelengde)
+        # Spiler
+        for j in range(2 * self.ant_spiler_i_høyden):
+          # Søk etter minste som er lang nok.
+          funnet_index = self.spiler_liste.finnMinsteLangNok(self.lengdespile_lengde)
+          if funnet_index == None:
+            print("init: fant ikke noen lange nok")
+            # Legg til ny full-lengde lekt
+            self.spiler_liste.leggTilNyLengde(SPILE_LENGDE)
+            # Øk teller på antall spiler nødvendig
+            self.antall_spiler_nødvendig += 1
+            print("init: Oppdaterte antall spiler nødvendig til " + str(self.antall_spiler_nødvendig))
+            # Søk igjen
+            funnet_index = self.spiler_liste.finnMinsteLangNok(self.lengdespile_lengde)
+          # Utfør kuttet
+          self.spiler_liste.kutt(funnet_index, self.lengdespile_lengde)
+        # Toppbord
+        for j in range(2):
+          # Søk etter minste som er lang nok.
+          funnet_index = self.toppbord_liste.finnMinsteLangNok(self.topp_lengde)
+          if funnet_index == None:
+            print("init: fant ikke noen lange nok")
+            # Legg til ny full-lengde toppbord
+            self.toppbord_liste.leggTilNyLengde(TOPPBORD_LENGDE)
+            # Øk teller på antall toppbord nødvendig
+            self.antall_toppbord_nødvendig += 1
+            print("init: Oppdaterte antall toppbord nødvendig til " + str(self.antall_toppbord_nødvendig))
+            # Søk igjen
+            funnet_index = self.toppbord_liste.finnMinsteLangNok(self.topp_lengde)
+          # Utfør kuttet
+          self.toppbord_liste.kutt(funnet_index, self.topp_lengde)
+      else:
+        # i == høyde
+        for j in range(self.ant_høydelekter):
+          # Søk etter minste som er lang nok.
+          funnet_index = self.lekter_liste.finnMinsteLangNok(self.høydelekter_lengde)
+          if funnet_index == None:
+            print("init: fant ikke noen lange nok")
+            # Legg til ny full-lengde lekt
+            self.lekter_liste.leggTilNyLengde(LEKT_LENGDE)
+            # Øk teller på antall lekter nødvendig
+            self.antall_lekter_nødvendig += 1
+            print("init: Oppdaterte antall lekter nødvendig til " + str(self.antall_lekter_nødvendig))
+            # Søk igjen
+            funnet_index = self.lekter_liste.finnMinsteLangNok(self.høydelekter_lengde)
+          # Utfør kuttet
+          self.lekter_liste.kutt(funnet_index, self.høydelekter_lengde)
+
 
   def print(self):
     lekter_dim_str = str(LEKT_BREDDE) + "x" + str(LEKT_HØYDE)
@@ -106,6 +370,21 @@ class Plantekasse:
     print(lekter_dim_str + ": " + str(int(self.lengde_lekter)) + " mm")
     print(spiler_dim_str + ": " + str(self.lengde_spiler) + " mm")
     print(toppbord_dim_str + ": " + str(self.lengde_toppbord) + " mm")
+    print("\n")
+    print("Antall lekter nødvendig av ulike dimensjoner:")
+    print(lekter_dim_str + ": " + str(self.antall_lekter_nødvendig) + " á " + str(LEKT_LENGDE) + " mm")
+    print(spiler_dim_str + ": " + str(self.antall_spiler_nødvendig) + " á " + str(SPILE_LENGDE) + " mm")
+    print(toppbord_dim_str + ": " + str(self.antall_toppbord_nødvendig) + " á " + str(TOPPBORD_LENGDE) + " mm")
+    print("\n")
+    print("Rest av lekter:")
+    self.lekter_liste.print()
+    print("\n")
+    print("Rest av spiler:")
+    self.spiler_liste.print()
+    print("\n")
+    print("Rest av toppbord:")
+    self.toppbord_liste.print()
+
 
 def main():
   parser = argparse.ArgumentParser(
